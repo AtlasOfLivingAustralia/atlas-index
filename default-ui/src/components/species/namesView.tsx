ca7ef17bdb1a594b4bafd5d291f1a81880bd8ba0
Nick dos Remedios <nick.dosremedios@csiro.au>
2024-09-09 16:04:13 +1000

    #6 species page - names tab

--- default-ui/src/components/species/namesView.tsx
+++ default-ui/src/components/species/namesView.tsx
@@ -1,102 +1,159 @@
+import { Anchor, Box, Flex, Table, Text, Title } from "@mantine/core";
+import DOMPurify from "dompurify";
+import classes from "./species.module.css";
+import { IconInfoCircleFilled } from "@tabler/icons-react";
+// import './species.module.css';
+
 interface MapViewProps {
-    result?: {},
-    resultV1?: {}
+    result?:  Record<PropertyKey, string | number | any >,
+    resultV1?:  Record<PropertyKey, string | number | any >
 }
 
 function NamesView({result, resultV1}: MapViewProps) {
 
     return <>
-        <div className="namesView">
+        <Box>
+            <Title order={3} mb="md" mt="md">Scientific names</Title>
+            <Table striped="even" mb="sm">
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Accepted name</Table.Th>
+                        <Table.Th>Source</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                <Table.Tr>
+                    <Table.Td>
+                        <Text 
+                            dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(
+                                    // TODO: find a better way to apply CSS rules
+                                    result?.nameFormatted
+                                        .replace(/class="scientific-name rank-subspecies"/g, `class="${classes.rankSpecies}"`)
+                                        .replace(/class="scientific-name rank-species"/g, `class="${classes.rankSpecies}"`)
+                                        .replace(/class="scientific-name rank-genus"/g, `class="${classes.rankSpecies}"`)
+                                        .replace(/class="name"/g, `class="${classes.name}"`)
+                                ) 
+                            }}
+                        ></Text>
+                    </Table.Td>
+                    <Table.Td><Anchor href={result?.source} target="_source">{result?.datasetName}</Anchor></Table.Td>
+                </Table.Tr>
+                {/* TODO: check for additional accepted names or remove this comment if not applicable */}
+                </Table.Tbody>
+            </Table>
+            <Table striped="even" mb="sm">
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Synonyms</Table.Th>
+                        <Table.Th>Source</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                    {resultV1?.synonyms && resultV1.synonyms.map((item, idx) =>
+                        <Table.Tr key={idx}>
+                            <Table.Td><Text dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(item?.nameFormatted)}}></Text></Table.Td>
+                        </Table.Tr>
+                    )}
+                </Table.Tbody>
+            </Table>
 
-            <div className="namesSectionHeader">Scientific</div>
-            <div className="namesRow d-flex">
-                <div className="namesRowName" dangerouslySetInnerHTML={{__html: result?.nameFormatted}}></div>
-                <div className="namesRowSource">{result?.datasetName}</div>
-            </div>
+            <Table striped="even" mb="sm">
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Variants</Table.Th>
+                        <Table.Th>Source</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                    {resultV1?.variants && resultV1.synonyms.map((item, idx) =>
+                        <Table.Tr key={idx}>
+                            <Table.Td><Text dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(item?.nameFormatted)}}></Text></Table.Td>
+                        </Table.Tr>
+                    )}
+                </Table.Tbody>
+            </Table>
 
-            <div className="namesSectionFoot"></div>
-            <div className="namesSectionHeader">Common</div>
-            {resultV1?.commonNames && resultV1.commonNames.map((item, idx) =>
-                item?.status !== 'traditionalKnowledge' &&
-                <div className={"namesRow d-flex " + (idx % 2 == 1 && "namesRowOdd")} key={idx}>
-                    <div className="namesRowName">{item?.nameString}</div>
-                    {item?.infoSourceURL ?
-                        <a href={item?.infoSourceURL} className="namesRowSource">{item?.infoSourceName}</a>
-                        :
-                        <div className="namesRowSource">{item?.infoSourceName}</div>
-                    }
-                </div>
-            )}
+            <Table striped="even" mb="sm">
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Identifiers</Table.Th>
+                        <Table.Th>Source</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                    {resultV1?.identifiers && resultV1.synonyms.map((item, idx) =>
+                        <Table.Tr key={idx}>
+                            <Table.Td><Text dangerouslySetInnerHTML={{
+                                __html: DOMPurify.sanitize(item?.nameFormatted)}}></Text></Table.Td>
+                        </Table.Tr>
+                    )}
+                </Table.Tbody>
+            </Table>
 
-            <div className="namesSectionFoot"></div>
-            <div className="namesSectionHeader">Indigenous</div>
-            <div className="namesInfo">
-                <div className="classificationAbout">
-                    <span className="bi bi-info-circle-fill"></span>
-                    About indigenous names
-                </div>
-                <div className="classificationInfoText">
-                    Classification of organisms allows us to group them and imply
-                    how they are related to each other. This includes a hierarchy
-                    of ranks e.g. kingdom, phylum etc. for more information see&nbsp;
-                    <span className="namesLink">An introduction to taxonomy</span>.
-                </div>
-            </div>
-            <div className="namesRow namesRowHeader d-flex">
-                <div className="namesRowHeaderLabelLeft">Name</div>
-                <div className="namesRowHeaderLabelRight">See language group</div>
-            </div>
-            {resultV1?.commonNames && resultV1.commonNames.map((item, idx) =>
-                item?.status === 'traditionalKnowledge' &&
-                <div className={"namesRow d-flex " + (idx % 2 == 1 && "namesRowOdd")} key={idx}>
-                    <div className="namesRowName">{item?.nameString}</div>
-                    {item?.infoSourceURL ?
-                        <a href={item?.infoSourceURL} className="namesRowSource">{item?.language}</a>
-                        :
-                        <div className="namesRowSource">{item?.language}</div>
-                    }
-                </div>
-            )}
+            <Title order={3} mb="md" mt="lg">Common names</Title>
+            <Table striped="even">
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Common name</Table.Th>
+                        <Table.Th>Source</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                {resultV1?.commonNames && resultV1.commonNames.map((item, idx) =>
+                    item?.status !== 'traditionalKnowledge' &&
+                    <Table.Tr key={idx}>
+                        <Table.Td>{item?.nameString}</Table.Td>
+                        <Table.Td>
+                            {item?.infoSourceURL ?
+                                <Anchor href={item?.infoSourceURL} target="_source">{item?.infoSourceName}</Anchor>
+                                :
+                                item?.infoSourceName
+                            }
+                        </Table.Td>
+                    </Table.Tr>
+                )}
+                </Table.Tbody>
+            </Table>
 
-            <div className="namesSectionFoot"></div>
-            <div className="namesSectionHeader">Synonyms</div>
-            {resultV1?.synonyms && resultV1.synonyms.map((item, idx) =>
-                <div className={"namesRow d-flex " + (idx % 2 == 1 && "namesRowOdd")} key={idx}>
-                    <div className="namesRowName" dangerouslySetInnerHTML={{__html: item?.nameFormatted}}></div>
-                    {item?.infoSourceURL ?
-                        <a href={item?.infoSourceURL} className="namesRowSource">{item?.infoSourceName || item?.nameAuthority}</a>
-                        :
-                        <div className="namesRowSource">{item?.infoSourceName}</div>
-                    }
-                </div>
-            )}
-
-            <div className="namesSectionFoot"></div>
-            <div className="namesSectionHeader">Variants</div>
-            {resultV1?.variants && resultV1.variants.map((item, idx) =>
-                <div className={"namesRow d-flex " + (idx % 2 == 1 && "namesRowOdd")} key={idx}>
-                    <div className="namesRowName" dangerouslySetInnerHTML={{__html: item?.nameFormatted}}></div>
-                    {item?.infoSourceURL ?
-                        <a href={item?.infoSourceURL} className="namesRowSource">{item?.infoSourceName}</a>
-                        :
-                        <div className="namesRowSource">{item?.infoSourceName}</div>
-                    }
-                </div>
-            )}
-
-            <div className="namesSectionFoot"></div>
-            <div className="namesSectionHeader">Identifiers</div>
-            {resultV1?.identifiers && resultV1.identifiers.map((item, idx) =>
-                <div className={"namesRow d-flex " + (idx % 2 == 1 && "namesRowOdd")} key={idx}>
-                    <div className="namesRowName">{item?.identifier}</div>
-                    {item?.infoSourceURL ?
-                        <a href={item?.infoSourceURL} className="namesRowSource">{item?.infoSourceName}</a>
-                        :
-                        <div className="namesRowSource">{item?.infoSourceName}</div>
-                    }
-                </div>
-            )}
-        </div>
+            <Title order={3} mb="md" mt="lg">Indigenous names</Title>
+            <Flex justify="flex-start" align="center" gap="xs" mb="sm">
+                <IconInfoCircleFilled size={18}/>
+                <Text fw={800} fz={16}>About indigenous names</Text>
+            </Flex>
+            <Text fz="sm">
+                The links from the Indigenous name provide more information about Indigenous Ecological
+                Knowledge (IEK) relating to the species. The link from language group links to the 
+                Australian Institute of Aboriginal and Torres Strait Islander Studies 
+                (<Anchor href="https://aiatsis.gov.au" target="_source">AIATSIS</Anchor>) 
+                information about the language. 
+            </Text>
+            <Table striped="even" mt="sm">
+                <Table.Thead>
+                    <Table.Tr>
+                        <Table.Th>Name</Table.Th>
+                        <Table.Th>See language group</Table.Th>
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                {resultV1?.commonNames && resultV1.commonNames.map((item, idx) =>
+                    item?.status === 'traditionalKnowledge' &&
+                    <Table.Tr key={idx}>
+                        <Table.Td>{item?.nameString}</Table.Td>
+                        <Table.Td>
+                            {item?.infoSourceURL ?
+                                <Anchor href={item?.infoSourceURL} target="_source">{item?.infoSourceName}</Anchor>
+                                :
+                                item?.infoSourceName
+                            }
+                        </Table.Td>
+                    </Table.Tr>
+                )}
+                </Table.Tbody>
+            </Table>
+        </Box>
     </>
 }
 
