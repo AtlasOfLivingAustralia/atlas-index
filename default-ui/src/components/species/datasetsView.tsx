cbbf49b0b80021a425c182166e0622dc9e7aa8e4
Nick dos Remedios <nick.dosremedios@csiro.au>
2024-09-11 14:58:58 +1000

    #6 species page - datasets tab

--- default-ui/src/components/species/datasetsView.tsx
+++ default-ui/src/components/species/datasetsView.tsx
@@ -1,16 +1,27 @@
 import {useEffect, useState} from "react";
+import { Anchor, Flex, Notification, Skeleton, Table, TableData, Text } from "@mantine/core";
+import { IconInfoCircleFilled } from "@tabler/icons-react";
 
 interface MapViewProps {
-    result?: {},
-    resultV1?: {}
+    result?:  Record<PropertyKey, string | number | any >,
+    resultV1?:  Record<PropertyKey, string | number | any >
+}
+
+interface Dataset {
+    name: string;
+    dataResourceUid: string;
+    licence: string;
+    records: number;
 }
 
 function DatasetsView({result, resultV1}: MapViewProps) {
 
-    const [datasets, setDatasets] = useState([]);
+    const [datasets, setDatasets] = useState<Dataset[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>('');
 
     // TODO: This should be fetched from a static source
-    // const datasets = [
+    // const datasets: Dataset[] = [
     //     {
     //         "name": "Atlas of Living Australia",
     //         "dataResourceUid" : "dr1",
@@ -42,10 +53,10 @@ function DatasetsView({result, resultV1}: MapViewProps) {
             return;
         }
 
+        setLoading(true);
+        setErrorMessage('');
         fetch("https://biocache.ala.org.au/ws/occurrences/search?q=lsid:\"" + encodeURIComponent(result.guid) + "\"&pageSize=0&facet=true&facets=dataResourceUid", {
-            // headers: {
-            //     'Content-Type': 'application/json'
-            // }
+            headers: { 'Content-Type': 'application/json' }
         })
             .then(response => response.json())
             .then(data => {
@@ -57,7 +68,8 @@ function DatasetsView({result, resultV1}: MapViewProps) {
                         newDatasets.push({
                             name: item.label,
                             dataResourceUid: dr,
-                            records: item.count
+                            records: item.count,
+                            licence: "" 
                         });
                         drs.push("id:" + dr)
                     }
@@ -80,42 +92,70 @@ function DatasetsView({result, resultV1}: MapViewProps) {
                         }
                         setDatasets(newDatasets);
                     })
+                    .catch(error => {
+                        setErrorMessage('Failed to fetch licenses - ' + error);
+                    })
+                    .finally(() => {
+                        // setLoading(false);
+                    });
             })
+            .catch(error => {
+                setErrorMessage('Failed to fetch datasets - ' + error);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     }, [result]);
 
-    return <>
-        <div className="datasetsView">
-            <div className="speciesMapAbout">
-                <span className="bi bi-info-circle-fill"></span>
-                About datasets
-            </div>
-            <div className="speciesMapText">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum sodales nunc, luctus libero
-                dictum vel. Donec placerat luctus eros, in iaculis ipsum pharetra volutpat. Sellus dictum nisi in
-                justo posuere, sit amet viverra lectus rutrum.
-            </div>
+    // Format the dataset table data for rendering
+    const [tableData, setTableData] = useState<TableData>({});
+    useEffect(() => {
+        setTableData({
+            head: ['Dataset', 'Licence', 'Records'],
+            body: datasets.map((item) => [
+                <Anchor target="_blank" href={"https://collections.ala.org.au/public/show/" + item.dataResourceUid}>
+                    {item.name}
+                </Anchor>,
+                item.licence,
+                <Anchor target="_blank" href={"https://biocache.ala.org.au/occurrences/search?q=lsid:\"" + result?.guid 
+                    + "\"&fq=dataResourceUid:" + item.dataResourceUid}>
+                    {item.records.toLocaleString()}
+                </Anchor>
+            ]),
+        });
+    }, [datasets]);
 
-            <div className="namesRowHeader datasetTable d-flex">
-                <div className="speciesTableHeaderItem datasetName">Dataset</div>
-                <div className="speciesTableHeaderItem datasetLicence">Licence</div>
-                <div className="speciesTableHeaderItem datasetRecords">Records</div>
-            </div>
-            {datasets.map((item, idx) =>
-                <div className={"namesRow d-flex " + (idx % 2 == 1 && "namesRowOdd")} key={idx}>
-                    <div className="speciesTableItem datasetName">
-                        <a target="_blank" href={"https://collections.ala.org.au/public/show/" + item.dataResourceUid}>
-                            {item.name}
-                        </a>
-                    </div>
-                    <div className="speciesTableItem datasetLicence">{item.licence}</div>
-                    <div className="speciesTableItem datasetRecords">
-                        <a target="_blank" href={"https://biocache.ala.org.au/occurrences/search?q=lsid:\"" + result.guid + "\"&fq=dataResourceUid:" + item.dataResourceUid}>
-                            {item.records.toLocaleString()}
-                        </a>
-                    </div>
-                </div>
-            )}
-        </div>
+    return <>
+        <Flex justify="flex-start" align="center" gap="xs" mb="sm">
+            <IconInfoCircleFilled size={18}/>
+            <Text fw="bold">About datasets</Text>
+        </Flex>
+        <Text mt="sm">
+            Much of the content in the Atlas of Living Australia, such as occurrence records, 
+            environmental data, images and the conservation status of species, comes from 
+            data sets provided by collecting institutions, government departments, individual 
+            collectors and community groups. 
+        </Text>
+        { loading && 
+            <Skeleton height={800} mt="lg" width="100%" radius="md" />
+        }
+        { errorMessage && 
+            <Notification 
+                withBorder 
+                mt="lg"
+                onClose={() => setErrorMessage('')}
+                title="Error loading datasets"
+            >
+                {errorMessage}
+            </Notification>
+        }
+        { datasets.length > 0 &&
+            <Table 
+                striped="even" 
+                data={tableData} 
+                mt="lg" 
+            />
+        }
     </>
 }
 
